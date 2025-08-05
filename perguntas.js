db61464d8ce612975a238cb03a8000920c8474e2
Makellycardoso2811 <kupsel.makelly@escola.pr.gov.br>
2025-08-05 10:48:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Agronomia',
+    'O que é agronomia?',
+    É a ciência e a arte de otimizar a produção de alimentos, fibras e energia de forma sustentável
+    )
+
+criaCartao(
+    'Agronomia',
+    'Quais são as funções susadas na agronomia?',
+    'Produção vegetal,Produção animal,Ciência do solo,Fitotecnia e agroecologia:'
+)
+
+criaCartao(
+    'Agricultura',
+    'O que é agricultura?',
+    ' é a prática de cultivar plantas e criar animais para produzir alimentos, fibras, combustível, ornamentos e outros produtos de origem vegetal.
+)
+
+criaCartao(
+    'Técnicas usadas tanto na Agronomia quanto na Agricultura',
+    'Tanto a agronomia quanto a agricultura utilizam diversas técnicas para otimizar a produção de alimentos e garantir a sustentabilidade. 
+    As principais técnicas incluem a rotação de culturas, o plantio direto, o uso de adubos orgânicos, 
+    a agricultura de precisão e o controle biológico de pragas. ?'
+    'Rotação de culturas,Plantio direto',
+)
